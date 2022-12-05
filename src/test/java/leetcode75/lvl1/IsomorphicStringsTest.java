6ecd26835d6d107803e0901119ac8200ce72a79e
pirog <vitalikvlasenko88@gmail.com>
2022-12-05 13:52:12 +0200

    done Isomorphic Strings

---
+++ src/test/java/leetcode75/lvl1/IsomorphicStringsTest.java
@@ -0,0 +1,29 @@
+package leetcode75.lvl1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IsomorphicStringsTest {
+
+    @Test
+    void isIsomorphic() {
+        IsomorphicStrings test = new IsomorphicStrings();
+
+        String s = "egg";
+        String t = "add";
+        assertTrue(test.isIsomorphic(s, t));
+
+        s = "foo";
+        t = "bar";
+        assertFalse(test.isIsomorphic(s, t));
+
+        t = "foo";
+        s = "bar";
+        assertFalse(test.isIsomorphic(s, t));
+
+        t = "paper";
+        s = "title";
+        assertTrue(test.isIsomorphic(s, t));
+    }
+}
