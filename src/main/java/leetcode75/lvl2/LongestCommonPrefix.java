31ab9b288824e4dc9b993c7fc2b30dcf429fbe20
Vitalik <vitalikvlasenko88@gmail.com>
2023-02-02 10:58:20 +0200

    done LongestCommonPrefix

---
+++ src/main/java/leetcode75/lvl2/LongestCommonPrefix.java
@@ -0,0 +1,19 @@
+package leetcode75.lvl2;
+
+
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < strs[0].length(); i++) {
+            char tmpChar = strs[0].charAt(i);
+            for (String s : strs) {
+                if (i == s.length() || s.charAt(i) != tmpChar) {
+                    return result.toString();
+                }
+            }
+            result.append(strs[0].charAt(i));
+        }
+        return result.toString();
+    }
+}
---
+++ src/test/java/leetcode75/lvl2/LongestCommonPrefixTest.java
@@ -0,0 +1,25 @@
+package leetcode75.lvl2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LongestCommonPrefixTest {
+
+    private final LongestCommonPrefix overClass = new LongestCommonPrefix();
+
+    @Test
+    void longestCommonPrefix() {
+        String[] words = {"flower","flow","flight"};
+        String expected = "fl";
+        assertEquals(expected, overClass.longestCommonPrefix(words));
+
+        words = new String[]{"dog","racecar","car"};
+        expected = "";
+        assertEquals(expected, overClass.longestCommonPrefix(words));
+
+        words = new String[]{"ab", "a"};
+        expected = "a";
+        assertEquals(expected, overClass.longestCommonPrefix(words));
+    }
+}
