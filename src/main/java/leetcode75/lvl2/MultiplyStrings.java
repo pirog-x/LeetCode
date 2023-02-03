3c0857e7ea41b0d1b25a41b3ee9742d68591454c
Vitalik <vitalikvlasenko88@gmail.com>
2023-02-03 12:03:44 +0200

    done MultiplyStrings

---
+++ src/main/java/leetcode75/lvl2/MultiplyStrings.java
@@ -0,0 +1,26 @@
+package leetcode75.lvl2;
+
+public class MultiplyStrings {
+    public String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) return "0";
+
+        int l1 = num1.length(), l2 = num2.length();
+        int[] res = new int[l1 + l2];
+
+        for (int i = l1 - 1; i >= 0; i--) {
+            for (int j = l2 - 1; j >= 0; j--) {
+                int sum = Character.getNumericValue(num1.charAt(i)) * Character.getNumericValue(num2.charAt(j));
+
+                sum += res[j + i + 1];
+                res[j + i + 1] = sum % 10;
+                res[j + i] += sum / 10;
+            }
+        }
+        StringBuilder builder = new StringBuilder();
+        for (int i : res) {
+            if (builder.isEmpty() && i == 0) continue;
+            builder.append(i);
+        }
+        return builder.toString();
+    }
+}
---
+++ src/test/java/leetcode75/lvl2/MultiplyStringsTest.java
@@ -0,0 +1,28 @@
+package leetcode75.lvl2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultiplyStringsTest {
+
+    MultiplyStrings testClass = new MultiplyStrings();
+
+    @Test
+    void multiply() {
+        String num1 = "123456789";
+        String num2 = "987654321";
+        String expected = "121932631112635269";
+        assertEquals(expected, testClass.multiply(num1, num2));
+
+        num1 = "3";
+        num2 = "2";
+        expected = "6";
+        assertEquals(expected, testClass.multiply(num1, num2));
+
+        num1 = "0";
+        num2 = "2";
+        expected = "0";
+        assertEquals(expected, testClass.multiply(num1, num2));
+    }
+}
