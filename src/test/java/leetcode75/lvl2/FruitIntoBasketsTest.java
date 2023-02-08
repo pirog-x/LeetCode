a617c3e0a27711f2dafb1497bf7536293699de1c
Vitalik <vitalikvlasenko88@gmail.com>
2023-02-08 15:00:46 +0200

    done FruitIntoBaskets

---
+++ src/test/java/leetcode75/lvl2/FruitIntoBasketsTest.java
@@ -0,0 +1,18 @@
+package leetcode75.lvl2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FruitIntoBasketsTest {
+
+    FruitIntoBaskets testClass = new FruitIntoBaskets();
+
+    @Test
+    void totalFruit() {
+        int[] fruits = new int[]{3,3,3,1,2,1,1,2,3,3,4};
+        int expected = 5;
+
+        assertEquals(expected, testClass.totalFruit(fruits));
+    }
+}
