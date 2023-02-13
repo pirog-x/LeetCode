26772b12f4ad40c0a46595e483fbe79a1d6f02a2
Vitalik <vitalikvlasenko88@gmail.com>
2023-02-13 12:05:42 +0200

    done LongestPalindromeByConcatenatingTwoLetterWords
    in fat case:
    time: O(n)
    space: O(n)

---
+++ src/test/java/leetcode75/lvl2/LongestPalindromeByConcatenatingTwoLetterWordsTest.java
@@ -0,0 +1,25 @@
+package leetcode75.lvl2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LongestPalindromeByConcatenatingTwoLetterWordsTest {
+
+    LongestPalindromeByConcatenatingTwoLetterWords testClass = new LongestPalindromeByConcatenatingTwoLetterWords();
+
+    @Test
+    void longestPalindrome() {
+        String[] words = {"lc","cl","gg"};
+        int expected = 6;
+        assertEquals(expected, testClass.longestPalindrome(words));
+
+        words = new String[]{"ab","ty","yt","lc","cl","ab"};
+        expected = 8;
+        assertEquals(expected, testClass.longestPalindrome(words));
+
+        words = new String[]{"cc","ll","xx"};
+        expected = 2;
+        assertEquals(expected, testClass.longestPalindrome(words));
+    }
+}
