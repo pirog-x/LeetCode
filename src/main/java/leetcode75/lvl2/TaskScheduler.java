a46352d5b860c675da9bf8f4c452fa2a7531d70d
Vitalik <vitalikvlasenko88@gmail.com>
2023-02-16 09:26:18 +0200

    done TaskScheduler

---
+++ src/main/java/leetcode75/lvl2/TaskScheduler.java
@@ -0,0 +1,28 @@
+package leetcode75.lvl2;
+
+// https://leetcode.com/problems/task-scheduler/?envType=study-plan&id=level-2
+// time: O(n * log(n))
+// space: O(n)
+
+import java.util.*;
+
+public class TaskScheduler {
+    public int leastInterval(char[] tasks, int n) {
+        if (n == 0) return tasks.length;
+
+        int[] charMap = new int[26];
+        for (char c : tasks) {
+            charMap[c - 'A']++;
+        }
+        Arrays.sort(charMap);
+
+        int maxVal = charMap[25] - 1;
+        int idleSlots = maxVal * n;
+
+        for (int i = 24; i >= 0; i--) {
+            idleSlots -= Math.min(charMap[i], maxVal);
+        }
+
+        return idleSlots > 0 ? idleSlots + tasks.length : tasks.length;
+    }
+}
---
+++ src/test/java/leetcode75/lvl2/TaskSchedulerTest.java
@@ -0,0 +1,31 @@
+package leetcode75.lvl2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskSchedulerTest {
+
+    TaskScheduler testClass = new TaskScheduler();
+
+    @Test
+    void leastInterval() {
+        char[] task = new char[] { 'A','A','A','B','B','B' };
+        int n = 2;
+        int expected = 8;
+
+        assertEquals(expected, testClass.leastInterval(task, n));
+
+        task = new char[] { 'A','A','A','B','B','B' };
+        n = 0;
+        expected = 6;
+
+        assertEquals(expected, testClass.leastInterval(task, n));
+
+        task = new char[] { 'A','A','A','A','A','A','B','C','D','E','F','G' };
+        n = 2;
+        expected = 16;
+
+        assertEquals(expected, testClass.leastInterval(task, n));
+    }
+}
