47f37e2236a7e6fa5bacce299eabaf87a3af5c83
Vitalik <vitalikvlasenko88@gmail.com>
2023-02-18 14:12:59 +0200

    done ClimbingStairs
    Memorization
    and
    Bottom-up

---
+++ src/main/java/top_interview_questions/easy/ClimbingStairs.java
@@ -0,0 +1,54 @@
+package top_interview_questions.easy;
+
+// https://leetcode.com/problems/climbing-stairs/
+
+// Memorization
+// time: O(n)
+// space: O(n)
+
+// Bottom-up
+// time: O(n)
+// space: O(1)
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ClimbingStairs {
+    private final Map<Integer, Integer> cache = new HashMap<>();
+
+    public int climbStairsBottomUp(int n) {
+        int one = 1;
+        int two = 1;
+        int tmp;
+
+        for (int i = 0; i < n - 1; i++) {
+            tmp = one;
+            one += two;
+            two = tmp;
+        }
+        return one;
+    }
+
+    public int climbStairs(int n) {
+        // bottom-up
+        return climbStairsBottomUp(n);
+        // memorization
+//        cache.clear();
+//        return go(0, n);
+    }
+
+    private int go(int step, int n) {
+        if (step > n) {
+            return 0;
+        }
+        if (step == n) {
+            return 1;
+        }
+        if (cache.containsKey(step)) {
+            return cache.get(step);
+        }
+        cache.put(step, go(step + 1, n) + go(step + 2, n));
+
+        return cache.get(step);
+    }
+}
---
+++ src/test/java/top_interview_questions/easy/ClimbingStairsTest.java
@@ -0,0 +1,21 @@
+package top_interview_questions.easy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClimbingStairsTest {
+
+    private final ClimbingStairs testClass = new ClimbingStairs();
+
+    @Test
+    void climbStairs() {
+        int stairs = 2;
+        int expected = 2;
+        assertEquals(expected, testClass.climbStairs(stairs));
+
+        stairs = 4;
+        expected = 5;
+        assertEquals(expected, testClass.climbStairs(stairs));
+    }
+}
