30499da23aa5a2ea9949e2f493150e7e35dd85d0
Vitalik <vitalikvlasenko88@gmail.com>
2023-02-19 12:13:42 +0200

    done WordSearch

---
+++ src/test/java/top_interview_questions/medium/WordSearchTest.java
@@ -0,0 +1,24 @@
+package top_interview_questions.medium;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WordSearchTest {
+
+    WordSearch testClass = new WordSearch();
+
+    @Test
+    void exist() {
+        char[][] board = {{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}};
+        String word = "ABCCED";
+        assertTrue(testClass.exist(board, word));
+    }
+
+    @Test
+    void notExist() {
+        char[][] board = {{'A','B','C','E'},{'S','F','C','S'}};
+        String word = "ABCB";
+        assertFalse(testClass.exist(board, word));
+    }
+}
