023380f9d08b22c30304e882f7b9d591561b48b9
Vitalik <vitalikvlasenko88@gmail.com>
2023-02-27 11:07:53 +0200

    add RunningSumOf1dArray.kt

--- src/main/kotlin/task/kotlin/leetcode75/lvl1/RunningSumOf1dArray.kt
+++ src/main/kotlin/task/kotlin/leetcode75/lvl1/RunningSumOf1dArray.kt
@@ -1,8 +1,18 @@
 package task.kotlin.leetcode75.lvl1
 
+// https://leetcode.com/problems/running-sum-of-1d-array
+// time: O(n)
+// space: O(1)
+
 class RunningSumOf1dArray {
     fun runningSum(nums: IntArray): IntArray {
+        if (nums.isEmpty()) return nums
+
+        var sumOfPrev = nums[0]
+        for (i in 1 until nums.size) {
+            nums[i] += sumOfPrev
+            sumOfPrev = nums[i]
+        }
         return nums
     }
-
 }
--- src/test/kotlin/task/kotlin/leetcode75/lvl1/RunningSumOf1dArrayTest.kt
+++ src/test/kotlin/task/kotlin/leetcode75/lvl1/RunningSumOf1dArrayTest.kt
@@ -8,8 +8,21 @@ class RunningSumOf1dArrayTest {
 
     @Test
     fun runningSumTest() {
-        val arr: IntArray = intArrayOf(1,2,3,4)
-        val expected: IntArray = intArrayOf(1,3,6,10)
+        // case: 1
+        var arr = intArrayOf(1,2,3,4)
+        var expected = intArrayOf(1,3,6,10)
+
+        Assertions.assertThat(testClass.runningSum(arr)).isEqualTo(expected)
+
+        // case: 2
+        arr = intArrayOf(1,1,1,1,1)
+        expected = intArrayOf(1,2,3,4,5)
+
+        Assertions.assertThat(testClass.runningSum(arr)).isEqualTo(expected)
+
+        // case: 3
+        arr = intArrayOf(3,1,2,10,1)
+        expected = intArrayOf(3,4,6,16,17)
 
         Assertions.assertThat(testClass.runningSum(arr)).isEqualTo(expected)
     }
