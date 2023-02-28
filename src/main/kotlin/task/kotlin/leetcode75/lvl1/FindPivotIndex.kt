f8a601ddeffdc60f52c21299cf8c73068e84c19c
Vitalik <vitalikvlasenko88@gmail.com>
2023-02-28 08:04:07 +0200

    add time complexity

--- src/main/kotlin/task/kotlin/leetcode75/lvl1/FindPivotIndex.kt
+++ src/main/kotlin/task/kotlin/leetcode75/lvl1/FindPivotIndex.kt
@@ -1,5 +1,9 @@
 package task.kotlin.leetcode75.lvl1
 
+// https://leetcode.com/problems/isomorphic-strings/?envType=study-plan&id=level-1
+// time: O(n)
+// space: O(1)
+
 class FindPivotIndex {
     fun pivotIndex(nums: IntArray): Int {
         var check = 0
